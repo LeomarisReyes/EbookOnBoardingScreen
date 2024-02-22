30cd41e123db39155a28faf11f930ee03beb4eee
Leomaris Reyes <reyes.leomaris@gmail.com>
2024-02-22 09:13:05 -0400

    Creating an E-Book OnBoarding screen in .NET MAUI

---
+++ EbookOnBoardingScreen/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EbookOnBoardingScreen;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
+
---
+++ EbookOnBoardingScreen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EbookOnBoardingScreen;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
